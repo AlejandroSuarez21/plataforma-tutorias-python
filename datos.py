91d6a39f04415785cf3ddc39d4eac97b3d682197
AlejandroSuarez21 <suarezrojas641@gmail.com>
2026-03-09 12:32:57 -0500

    Update datos.py

--- datos.py
+++ datos.py
@@ -11,5 +11,16 @@ def agregar_estudiante(nombre):
 def obtener_tutores():
     return tutores
 
+def buscar_por_materia(materia):
+    resultado = []
+    for tutor in tutores:
+        if tutor[1].lower() == materia.lower():
+            resultado.append(tutor)
+    return resultado
+
 def agendar_tutoria(estudiante, tutor, materia):
     tutorias.append((estudiante, tutor, materia))
+
+    archivo = open("tutorias.txt","a")
+    archivo.write(estudiante + "," + tutor + "," + materia + "\n")
+    archivo.close()
